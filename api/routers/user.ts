3bb2297fc78e23b4abf1f75d522c0938730d579c
arkan <fra@crtweb.ru>
2020-07-14 23:29:06 +0500

    asd

--- api/routers/user.ts
+++ api/routers/user.ts
@@ -1,40 +1,23 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response } from "express";
 import UserService from "../services/user";
 
 const userRouter = Router();
 
-userRouter.get('/', async (req: Request, res: Response) => {
-    const { count, rows } = await UserService.userList();
-    if (!count) {
-        return res.status(204).json({
+userRouter.get("/", async (req: Request, res: Response) => {
+  const userList = await UserService.userList();
+  if (!userList.rows.length) {
+    return res.status(204).json(userList);
+  }
+  return res.status(200).json(userList);
+});
 
-        });
-    }
-    return res.status(200).json({
-        count,
-        users: rows
-    });
-})
-
-userRouter.post('/', async (req: Request, res: Response) => {
-    try {
-        const newUser = await UserService.createUser(req.body);
-        return res.status(200).json(newUser);
-    } catch (e) {
-        return res.status(500).json(e);
-    }
-})
-
-userRouter.get('/:id', async (req: Request, res: Response) => {
-    try {
-        const user = await UserService.getUserById(Number(req.params.id));
-        if (user) {
-            return res.json(user);
-        }
-        return res.status(204).json(user);
-    } catch (e) {
-        return res.status(500).json(e);
-    }
+userRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const newUser = await UserService.createUser(req.body);
+    return res.status(200).json(newUser);
+  } catch (e) {
+    return res.status(500).json(e);
+  }
 });
 
 export default userRouter;
