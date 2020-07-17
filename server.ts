6a54c6387bec69af78915b38af0dd9dc4cd2ee31
Arkdiy <fra@crtweb.ru>
2020-07-17 18:10:29 +0500

    cors

--- server.ts
+++ server.ts
@@ -2,12 +2,14 @@ import express from "express";
 import chalk from "chalk";
 import apiRouter from "./api/routers";
 import bodyParser from "body-parser";
+import cors from 'cors';
 import { version } from "./package.json";
 
 const { log } = console;
 
 const server = express();
 const apiVer = version.split(".")[0];
+server.use(cors());
 server.disable("x-powered-by");
 server.set("views", __dirname + "/views");
 server.set("view engine", "pug");
